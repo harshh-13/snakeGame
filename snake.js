22259c17cce4b73763435fb618967ffaefa27765
harshi-reddy <harshavikasamarapaku@gmail.com>
2021-07-11 18:29:40 +0530

    Updated files

--- snake.js
+++ snake.js
@@ -1,17 +1,11 @@
-
-
-
-
 function init(){
 	canvas = document.getElementById('mycanvas');
 	W = H = canvas.width = canvas.height = 1000;
 	pen = canvas.getContext('2d');
-	cs = 66;
+	cs = 67;
 	game_over = false;
 	score = 5;
 
-
-	//Create a Image Object for food
 	food_img = new Image();
 	food_img.src = "Assets/apple.png";
 
@@ -25,15 +19,12 @@ function init(){
 		color:"blue",
 		cells:[],
 		direction:"right",
-
-
 		createSnake:function(){
 			for(var i=this.init_len;i>0;i--){
 				this.cells.push({x:i,y:0});
 			}
 		},
 		drawSnake:function(){
-
 			for(var i=0;i<this.cells.length;i++){
 				pen.fillStyle = this.color;
 				pen.fillRect(this.cells[i].x*cs,this.cells[i].y*cs,cs-3,cs-3);
@@ -41,9 +32,6 @@ function init(){
 		},
 
 		updateSnake:function(){
-			//console.log("updating snake according to the direction property");
-			//check if the snake has eaten food, increase the length of the snake and 
-			//generate new food object
 			var headX = this.cells[0].x;
 			var headY = this.cells[0].y;
 
@@ -53,14 +41,11 @@ function init(){
 				score++;
 
 			}
-			else
-			{
+			else {
 				this.cells.pop();
 			}
 
-			
 			var nextX,nextY;
-
 			if(this.direction=="right"){
 				nextX = headX + 1;
 				nextY = headY;
@@ -80,22 +65,17 @@ function init(){
 
 			this.cells.unshift({x: nextX,y:nextY});
 
-			/*Write a Logic that prevents snake from going out*/
 			var last_x = Math.round(W/cs);
 			var last_y = Math.round(H/cs);
 
 			if(this.cells[0].y<0 || this.cells[0].x < 0 || this.cells[0].x > last_x || this.cells[0].y > last_y){
 				game_over = true;
 			}
-
 		}
-
 	};
 
 	snake.createSnake();
-	//Add a Event Listener on the Document Object
 	function keyPressed(e){
-		//Conditional Statments
 		if(e.key=="ArrowRight"){
 			snake.direction = "right";
 		}
@@ -108,19 +88,11 @@ function init(){
 		else{
 			snake.direction = "up";
 		}
-		console.log(snake.direction);
 	}
-
-
-	document.addEventListener('keydown',keyPressed) ;
-	
+	document.addEventListener('keydown',keyPressed) ;	
 }
 
-
 function draw(){
-	//console.log("In Draw");
-
-	//erase the old frame
 	pen.clearRect(0,0,W,H);
 	snake.drawSnake();
 
@@ -131,17 +103,13 @@ function draw(){
 	pen.fillStyle = "blue";
 	pen.font = "20px Roboto"
 	pen.fillText(score,50,50);
-
-	
 }
 
 function update(){
-	//console.log("In Update");
 	snake.updateSnake(); 
 }
 
 function getRandomFood(){
-
 	var foodX = Math.round(Math.random()*(W-cs)/cs);
 	var foodY = Math.round(Math.random()*(H-cs)/cs);
 
@@ -151,7 +119,6 @@ function getRandomFood(){
 		color:"red",
 	}
 	return food
-
 }
 
 function gameloop(){
@@ -167,14 +134,3 @@ function gameloop(){
 init();
 
 var f = setInterval(gameloop,100);
-
-
-
-
-
-
-
-
-
-
-
